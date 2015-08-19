568203b7472933dceef7c1bfe5eabede2e569b18
Gines Fernandez <giferan21@gmail.com>
2015-08-19 12:42:51 +0200

    ObligatorioT9

--- app.js
+++ app.js
@@ -48,6 +48,29 @@ app.use(function(req, res, next) {
 });
 
 
+// Auto log-out
+app.use(function(req, res, next) {
+
+  if(req.session.user){
+    var ahora = new Date().getTime();
+    console.log(ahora,"-",req.session.time, "=",ahora-req.session.time );
+    if(ahora-req.session.time>= 180000){ //180000 = 60s * 2 * 1000
+      delete req.session.user;
+    }
+    req.session.time = ahora;
+
+  }
+
+  next();
+
+});
+
+
+
+
+
+
+
 app.use('/', routes);
 
 
